e3a6273d97e60266e7d7c9e17955f43743e9d0e4
pebsisab <isap@tlu.ee>
2023-03-17 13:02:57 +0200

    13:00 seisuga kt

---
+++ 05_kontrolltoo1/Kontrollt66_ip/src/Toiduaine.java
@@ -0,0 +1,16 @@
+public class Toiduaine {
+    String nimetus;
+    int valkudeProtsent;
+    int rasvadeProtsent;
+    int sysivesikuteProtsent;
+
+    public Toiduaine(String nimetus, int valkudeProtsent, int rasvadeProtsent, int sysivesikuteProtsent){
+        this.nimetus = nimetus;
+        this.valkudeProtsent = valkudeProtsent;
+        this.rasvadeProtsent = rasvadeProtsent;
+        this.sysivesikuteProtsent = sysivesikuteProtsent;
+    }
+
+
+
+}
---
+++ 05_kontrolltoo1/Kontrollt66_ip/src/Toidukomponent.java
@@ -0,0 +1,11 @@
+public class Toidukomponent {
+    Toiduaine toiduaine;
+    int kogusGrammides;
+
+    public Toidukomponent(Toiduaine toiduaine, int kogusGrammides){
+        this.toiduaine = toiduaine;
+        this.kogusGrammides = kogusGrammides;
+    }
+
+    //rasvakoguse arvutamine
+}
